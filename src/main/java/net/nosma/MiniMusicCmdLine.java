db432b121f8b11382707417db3a11f14fd11941d
redEkim <mikhail6886@yandex.ru>
2025-04-17 10:03:28 +0500

    added music and gui class

--- src/main/java/net/nosma/MiniMusicCmdLine.java
+++ src/main/java/net/nosma/MiniMusicCmdLine.java
@@ -28,7 +28,17 @@ public class MiniMusicCmdLine {
             MidiEvent changeInstrument = new MidiEvent(first, 1);
             track.add(changeInstrument);
 
-            //
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, note, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, note, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+            player.setSequence(seq);
+            player.start();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
