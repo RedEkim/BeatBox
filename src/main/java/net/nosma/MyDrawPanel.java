1f8183eeb656c14b7d3e1e24f5897eb4ea674329
redEkim <mikhail6886@yandex.ru>
2025-04-18 14:46:25 +0500

    added gui class with draw panel

--- src/main/java/net/nosma/MyDrawPanel.java
+++ src/main/java/net/nosma/MyDrawPanel.java
@@ -5,7 +5,14 @@ import java.awt.*;
 
 public class MyDrawPanel extends JPanel {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        frame.add(new MyDrawPanel());
+
+        frame.setSize(300, 300);
+
+        frame.setVisible(true);
     }
 //    public void paintComponent(Graphics g) {
 //        g.setColor(Color.orange);
@@ -17,16 +24,34 @@ public class MyDrawPanel extends JPanel {
 //        g.drawImage(image,3,4,this);
 //    }
 
+//    public void paintComponent(Graphics g) {
+//        g.fillRect(0,0, this.getWidth(), this.getHeight());
+//
+//        int red = (int) (Math.random() * 255);
+//        int green = (int) (Math.random() * 255);
+//        int blue = (int) (Math.random() * 255);
+//
+//        Color randomColor = new Color(red, green, blue);
+//        g.setColor(randomColor);
+//        g.fillOval(70,70,100,100);
+//    }
+
     public void paintComponent(Graphics g) {
-        g.fillRect(0,0, this.getWidth(), this.getHeight());
+        Graphics2D g2d = (Graphics2D) g;
 
         int red = (int) (Math.random() * 255);
         int green = (int) (Math.random() * 255);
         int blue = (int) (Math.random() * 255);
+        Color startColor = new Color(red, green, blue);
 
-        Color randomColor = new Color(red, green, blue);
-        g.setColor(randomColor);
-        g.fillOval(70,70,100,100);
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endColor = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
     }
 
 }
