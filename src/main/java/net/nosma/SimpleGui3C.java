0aca2e1b1225298cae6b250d1258d2b8632841cc
redEkim <mikhail6886@yandex.ru>
2025-04-22 09:10:01 +0500

    added gui3 class with draw panel and button(by click you can change color of shape)

---
+++ src/main/java/net/nosma/SimpleGui3C.java
@@ -0,0 +1,32 @@
+package net.nosma;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui3C implements ActionListener {
+    JFrame frame;
+    MyDrawPanel drawPanel = new MyDrawPanel();
+    public static void main(String[] args) {
+        SimpleGui3C gui = new SimpleGui3C();
+        gui.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("change colors");
+        button.addActionListener(this);
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent event) {
+        frame.repaint();
+    }
+}
