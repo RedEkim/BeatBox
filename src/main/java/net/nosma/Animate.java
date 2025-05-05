3ba7855969e9e536f89647004c9eba4797046043
redEkim <mikhail6886@yandex.ru>
2025-05-05 08:47:14 +0500

    added Animate.class from exercise in the end of chapter

---
+++ src/main/java/net/nosma/Animate.java
@@ -0,0 +1,25 @@
+package net.nosma;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Animate {
+    int x = 1;
+    int y = 1;
+
+    public static void main(String[] args) {
+        Animate gui = new Animate();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    class MyDrawP extends JPanel {
+        public void paintComponent(Graphics g) {
+
+        }
+    }
+}
