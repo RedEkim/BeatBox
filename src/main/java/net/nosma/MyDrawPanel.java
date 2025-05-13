2253c420efcc264a9d307e23e25bf9be45c94a04
redEkim <mikhail6886@yandex.ru>
2025-05-13 09:00:14 +0500

    updated class, visible of image

--- src/main/java/net/nosma/MyDrawPanel.java
+++ src/main/java/net/nosma/MyDrawPanel.java
@@ -22,10 +22,10 @@ public class MyDrawPanel extends JPanel {
 //        g.fillRect(20, 50, 100, 100);
 //    }
 
-//    public void paintComponent(Graphics g) {
-//        Image image = new ImageIcon("Dog.jpg").getImage();
-//        g.drawImage(image,3,4,this);
-//    }
+    public void paintComponent(Graphics g) {
+        Image image = new ImageIcon("src/main/java/net/nosma/Dog.jpg").getImage();
+        g.drawImage(image,3,4,this);
+    }
 
 //    public void paintComponent(Graphics g) {
 //        g.fillRect(0,0, this.getWidth(), this.getHeight());
@@ -39,22 +39,22 @@ public class MyDrawPanel extends JPanel {
 //        g.fillOval(70,70,100,100);
 //    }
 
-    public void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-
-        int red = (int) (Math.random() * 255);
-        int green = (int) (Math.random() * 255);
-        int blue = (int) (Math.random() * 255);
-        Color startColor = new Color(red, green, blue);
-
-        red = (int) (Math.random() * 255);
-        green = (int) (Math.random() * 255);
-        blue = (int) (Math.random() * 255);
-        Color endColor = new Color(red, green, blue);
-
-        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
-        g2d.setPaint(gradient);
-        g2d.fillOval(70, 70, 100, 100);
-    }
+//    public void paintComponent(Graphics g) {
+//        Graphics2D g2d = (Graphics2D) g;
+//
+//        int red = (int) (Math.random() * 255);
+//        int green = (int) (Math.random() * 255);
+//        int blue = (int) (Math.random() * 255);
+//        Color startColor = new Color(red, green, blue);
+//
+//        red = (int) (Math.random() * 255);
+//        green = (int) (Math.random() * 255);
+//        blue = (int) (Math.random() * 255);
+//        Color endColor = new Color(red, green, blue);
+//
+//        GradientPaint gradient = new GradientPaint(70, 70, startColor, 150, 150, endColor);
+//        g2d.setPaint(gradient);
+//        g2d.fillOval(70, 70, 100, 100);
+//    }
 
 }
