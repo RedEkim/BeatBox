00664aa9029e5385abc13313ade322a2cd6a7974
redEkim <mikhail6886@yandex.ru>
2025-05-26 08:57:56 +0500

    updated class with BorderLayout

--- src/main/java/net/nosma/chapter13/Button1.java
+++ src/main/java/net/nosma/chapter13/Button1.java
@@ -11,10 +11,23 @@ public class Button1 {
 
     public void go() {
         JFrame frame = new JFrame();
-        JButton button = new JButton("click me");
-        frame.getContentPane().add(BorderLayout.EAST, button);
+
+        JButton east = new JButton("EAST");
+        JButton west = new JButton("WEST");
+        JButton north = new JButton("NORTH");
+        JButton south = new JButton("SOUTH");
+        JButton center = new JButton("CENTER");
+
+
+
+        frame.getContentPane().add(BorderLayout.EAST, east);
+        frame.getContentPane().add(BorderLayout.WEST, west);
+        frame.getContentPane().add(BorderLayout.NORTH, north);
+        frame.getContentPane().add(BorderLayout.SOUTH, south);
+        frame.getContentPane().add(BorderLayout.CENTER, center);
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(200,200);
+        frame.setSize(300,300);
         frame.setVisible(true);
     }
 }
